df42584a96a5bc370ee8a8475261599e5c1b2f26
Cripa Varghese <cripav96@gmail.com>
2023-10-24 23:41:28 +0530

    feature: added styles

---
+++ src/style.ts
@@ -0,0 +1,19 @@
+const font = {
+  fontSize: "xs",
+};
+
+export const footerStyles = {
+  headingStyle: {
+    fontSize: "11",
+    fontWeight: "bold",
+  },
+  listItemStyle: {
+    color: "#7A7C88",
+    fontSize: "sm",
+  },
+};
+
+export const style = {
+  font,
+  footerStyles,
+};
