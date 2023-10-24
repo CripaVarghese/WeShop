2257612450ff87ad0b56c63134027b0118fb2981
Cripa Varghese <cripav96@gmail.com>
2023-10-24 23:40:17 +0530

    feature: added data for the footer

---
+++ src/footerData/online_Data.ts
@@ -0,0 +1,32 @@
+const online_Data = [
+  {
+    label: "Men",
+    link: "#",
+  },
+  {
+    label: "Women",
+    link: "#",
+  },
+  {
+    label: "Kids",
+    link: "#",
+  },
+  {
+    label: "Home & Living",
+    link: "#",
+  },
+  {
+    label: "Beauty",
+    link: "#",
+  },
+  {
+    label: "Gift Cards",
+    link: "#",
+  },
+  {
+    label: "Myntra Insider",
+    link: "#",
+  },
+];
+
+export default online_Data;
---
+++ src/footerData/usefulLinks_Data.ts
@@ -0,0 +1,24 @@
+const usefulLinks_Data = [
+  {
+    label: "Blog",
+    link: "#",
+  },
+  {
+    label: "Careers",
+    link: "#",
+  },
+  {
+    label: "Site Map",
+    link: "#",
+  },
+  {
+    label: "Corporate Information",
+    link: "#",
+  },
+  {
+    label: "White hat",
+    link: "#",
+  },
+];
+
+export default usefulLinks_Data;
