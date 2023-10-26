7692b2d4c41cb4b7d9abc02a4772b874b0516e6d
Cripa Varghese <cripav96@gmail.com>
2023-10-26 23:20:48 +0530

    feature: added data

---
+++ src/constants/menDropdownOptions.ts
@@ -0,0 +1,247 @@
+export const menOptions = [
+  // topWear
+  {
+    label: "T-Shirts",
+    link: "#",
+  },
+  {
+    label: "Casual Shirts",
+    link: "#",
+  },
+  {
+    label: "Formal Shirts",
+    link: "#",
+  },
+  {
+    label: "Sweatshirts",
+    link: "#",
+  },
+  {
+    label: "Sweaters",
+    link: "#",
+  },
+  {
+    label: "Jackets",
+    link: "#",
+  },
+  {
+    label: "Blazers & Coats",
+    link: "#",
+  },
+  {
+    label: "Suits",
+    link: "#",
+  },
+  {
+    label: "Rain Jackets",
+    link: "#",
+  },
+
+  // Indian & Festive Wear
+  {
+    label: "Kurtas & Kurta Sets",
+    link: "#",
+  },
+  {
+    label: "Sherwanis",
+    link: "#",
+  },
+  {
+    label: "Nehru Jackets",
+    link: "#",
+  },
+  {
+    label: "Dhotis",
+    link: "#",
+  },
+
+  // Bottomwear
+  {
+    label: "Jeans",
+    link: "#",
+  },
+  {
+    label: "Casual Trousers",
+    link: "#",
+  },
+  {
+    label: "Formal Trousers",
+    link: "#",
+  },
+  {
+    label: "Shorts",
+    link: "#",
+  },
+  {
+    label: "Track Pants & Joggers",
+    link: "#",
+  },
+
+  // Innerwear & Sleepwear
+  {
+    label: "Briefs & Trunks",
+    link: "#",
+  },
+  {
+    label: "Boxers",
+    link: "#",
+  },
+  {
+    label: "Vests",
+    link: "#",
+  },
+  {
+    label: "Sleepwear & Loungewear",
+    link: "#",
+  },
+  {
+    label: "Thermals",
+    link: "#",
+  },
+
+  //  Plus Size
+  // *
+
+  //   Footwear
+  {
+    label: "Casual Shoes",
+    link: "#",
+  },
+  {
+    label: "Sports Shoes",
+    link: "#",
+  },
+  {
+    label: "Formal Shoes",
+    link: "#",
+  },
+  {
+    label: "Sneakers",
+    link: "#",
+  },
+  {
+    label: "Sandals & Floaters",
+    link: "#",
+  },
+  {
+    label: "Flip Flops",
+    link: "#",
+  },
+  {
+    label: "Socks",
+    link: "#",
+  },
+
+  // *
+  //   Personal Care & Grooming
+  // Sunglasses & Frames
+  // Watches
+  // *
+
+  // Sports & Active Wear
+  {
+    label: "Sports Shoes",
+    link: "#",
+  },
+  {
+    label: "Sports Sandals",
+    link: "#",
+  },
+  {
+    label: "Active T-Shirts",
+    link: "#",
+  },
+  {
+    label: "Track Pants & Shorts",
+    link: "#",
+  },
+  {
+    label: "Tracksuits",
+    link: "#",
+  },
+  {
+    label: "Jackets & Sweatshirts",
+    link: "#",
+  },
+  {
+    label: "Sports Accessories",
+    link: "#",
+  },
+  {
+    label: "Swimwear",
+    link: "#",
+  },
+
+  // Gadgets
+  {
+    label: "Smart Wearables",
+    link: "#",
+  },
+  {
+    label: "Fitness Gadgets",
+    link: "#",
+  },
+  {
+    label: "Headphones",
+    link: "#",
+  },
+  {
+    label: "Speakers",
+    link: "#",
+  },
+
+  // Fashion Accessories
+  {
+    label: "Wallets",
+    link: "#",
+  },
+  {
+    label: "Belts",
+    link: "#",
+  },
+  {
+    label: "Perfumes & Body Mists",
+    link: "#",
+  },
+  {
+    label: "Trimmers",
+    link: "#",
+  },
+  {
+    label: "Deodorants",
+    link: "#",
+  },
+  {
+    label: "Ties, Cufflinks & Pocket Squares",
+    link: "#",
+  },
+  {
+    label: "Accessory Gift Sets",
+    link: "#",
+  },
+  {
+    label: "Caps & Hats",
+    link: "#",
+  },
+  {
+    label: "Mufflers, Scarves & Gloves",
+    link: "#",
+  },
+  {
+    label: "Phone Cases",
+    link: "#",
+  },
+  {
+    label: "Rings & Wristwear",
+    link: "#",
+  },
+  {
+    label: "Helmets",
+    link: "#",
+  },
+
+  // *
+  //   Bags & Backpacks
+  // Luggages & Trolleys
+  // *
+];
---
+++ src/constants/womenDropdownOption.ts
@@ -0,0 +1,227 @@
+export const womenOptions = [
+  // Indian & Fusion Wear
+  {
+    label: "Kurtas & Suits",
+    link: "#",
+  },
+  {
+    label: "Kurtis, Tunics & Tops",
+    link: "#",
+  },
+  {
+    label: "Sarees",
+    link: "#",
+  },
+  {
+    label: "Ethnic Wear",
+    link: "#",
+  },
+  {
+    label: "Leggings, Salwars & Churidars",
+    link: "#",
+  },
+  {
+    label: "Skirts & Palazzos",
+    link: "#",
+  },
+  {
+    label: "Dress Materials",
+    link: "#",
+  },
+  {
+    label: "Lehenga Cholis",
+    link: "#",
+  },
+  {
+    label: "Dupattas & Shawls",
+    link: "#",
+  },
+  {
+    label: "Jackets",
+    link: "#",
+  },
+
+  // *
+  // Belts, Scarves & More
+  // Watches & Wearables
+  // *
+
+  // Western Wear
+  {
+    label: "Tops",
+    link: "#",
+  },
+  {
+    label: "Tshirts",
+    link: "#",
+  },
+  {
+    label: "Jeans",
+    link: "#",
+  },
+  {
+    label: "Trousers & Capris",
+    link: "#",
+  },
+  {
+    label: "Shorts & Skirts",
+    link: "#",
+  },
+  {
+    label: "Co-ords",
+    link: "#",
+  },
+  {
+    label: "Playsuits",
+    link: "#",
+  },
+  {
+    label: "Jumpsuits",
+    link: "#",
+  },
+  {
+    label: "Shrugs",
+    link: "#",
+  },
+  {
+    label: "Sweaters & Sweatshirts",
+    link: "#",
+  },
+  {
+    label: "Jackets & Coats",
+    link: "#",
+  },
+  {
+    label: "Blazers & Waistcoats",
+    link: "#",
+  },
+
+  // Maternity
+  // Sunglasses & Frames
+
+  // Footwear
+  {
+    label: "Flats",
+    link: "#",
+  },
+  {
+    label: "Casual Shoes",
+    link: "#",
+  },
+  {
+    label: "Heels",
+    link: "#",
+  },
+  {
+    label: "Boots",
+    link: "#",
+  },
+  {
+    label: "Sports Shoes & Floaters",
+    link: "#",
+  },
+
+  // Sports & Active Wear
+  {
+    label: "Clothing",
+    link: "#",
+  },
+  {
+    label: "Footwear",
+    link: "#",
+  },
+  {
+    label: "Sports Accessories",
+    link: "#",
+  },
+  {
+    label: "Sports Equipment",
+    link: "#",
+  },
+
+  // Lingerie & Sleepwear
+  {
+    label: "Bra",
+    link: "#",
+  },
+  {
+    label: "Briefs",
+    link: "#",
+  },
+  {
+    label: "Shapewear",
+    link: "#",
+  },
+  {
+    label: " Sleepwear & Loungewear",
+    link: "#",
+  },
+  {
+    label: "Swimwear",
+    link: "#",
+  },
+  {
+    label: "Camisoles & Thermals",
+    link: "#",
+  },
+
+  // Beauty & Personal Care
+  {
+    label: "Makeup",
+    link: "#",
+  },
+  {
+    label: "Skincare",
+    link: "#",
+  },
+  {
+    label: "Premium Beauty",
+    link: "#",
+  },
+  {
+    label: "Lipsticks",
+    link: "#",
+  },
+  {
+    label: "Fragrances",
+    link: "#",
+  },
+
+  // Gadgets
+  {
+    label: "Smart Wearables",
+    link: "#",
+  },
+  {
+    label: "Fitness Gadgets",
+    link: "#",
+  },
+  {
+    label: "Headphones",
+    link: "#",
+  },
+  {
+    label: "Speakers",
+    link: "#",
+  },
+
+  // Jewellery
+  {
+    label: "Fashion Jewellery",
+    link: "#",
+  },
+  {
+    label: "Fine Jewellery",
+    link: "#",
+  },
+  {
+    label: "Earrings",
+    link: "#",
+  },
+
+  //*
+  // Backpacks
+  // Handbags, Bags & Wallets
+  // Luggages & Trolleys
+  //*
+];
