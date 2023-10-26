41f95e21221cefcfbf28713165f36ac481c2de56
Cripa Varghese <cripav96@gmail.com>
2023-10-26 23:23:16 +0530

    feature: added data

---
+++ src/constants/navbarItems.ts
@@ -0,0 +1,40 @@
+export const navbarItems = [
+  {
+    title: "MEN",
+    url: "/men",
+    submenu: [
+      {
+        title: "T-Shirts",
+        url: "#",
+      },
+      {
+        title: "Casual Shirts",
+        url: "#",
+      },
+      {
+        title: "Formal Shirts",
+        url: "#",
+      },
+    ],
+  },
+  {
+    title: "WOMEN",
+    url: "/women",
+  },
+  {
+    title: "KIDS",
+    url: "/kids",
+  },
+  {
+    title: "HOME & LIVING",
+    url: "/home_and_living",
+  },
+  {
+    title: "BEAUTY",
+    url: "/beauty",
+  },
+  {
+    title: "STUDIO",
+    url: "/studio",
+  },
+];
