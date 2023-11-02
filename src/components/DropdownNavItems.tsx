c663a15e49706b88626e8af02fdeb073eb708507
Cripa Varghese <cripav96@gmail.com>
2023-11-02 23:59:26 +0530

    modified the component

--- src/components/DropdownNavItems.tsx
+++ src/components/DropdownNavItems.tsx
@@ -1,36 +1,67 @@
-import { Stack, Box, Grid, Divider} from '@chakra-ui/react'
+import { Stack, Box, Grid, Divider } from "@chakra-ui/react";
+import { CategoryOptions } from "../constants/navbarOptionsData";
 
-const DropdownNavItems = ({data}:any) => {
+const NavItem = (props: {
+  data: { url?: string; label: any };
+  color?: string;
+}) => (
+  <Box
+    fontSize={"13px"}
+    py={"2px"}
+    _hover={{ fontWeight: "bold" }}
+    marginTop={0}
+    color={props.color}
+  >
+    <a href={props.data.url ?? "#"}>{props.data.label}</a>
+  </Box>
+);
+
+const DropdownNavItems = ({ data }: { data: CategoryOptions }) => {
   return (
-    <Box position='absolute' top='80px' right='100px' width='83%' height='440px' backgroundColor='white' p={'15px 40px'}>
-          <Grid templateColumns='repeat(5, 1fr)'>
-            {data.navColumns.map((navColumns, navColumnsIndex) => (
-              <Stack key={navColumnsIndex} spacing={0}>
-                {navColumns.categories.map((navColumn, navColumnIndex) => (
-                  <Box key={navColumnIndex}>
-                    {navColumn.categories.map((navGroup, navGroupIndex) => (
-                    <Box key={navGroupIndex} pt={'7px'}>
-                      <Box fontSize={'13px'} fontWeight={'bold'} py={'2px'} color={"#ee5f73"}>
-                        <a href={navGroup.header.url}>{navGroup.header.label}</a>
-                      </Box>
-                      <Stack spacing={0}>
-                          {navGroup.menuItems?.map((navGroupItem: { url: string | undefined; label: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined }, navGroupItemIndex: Key | null | undefined) => (
-                            <Box key={navGroupItemIndex} fontSize={'13px'} py={'2px'} _hover={{ fontWeight: 'bold' }} marginTop={0}>
-                              <a href={navGroupItem.url}>{navGroupItem.label}</a>
-                            </Box>
+    <Box
+      position="absolute"
+      top="80px"
+      right="100px"
+      width="83%"
+      height="440px"
+      backgroundColor="white"
+      p={"15px 40px"}
+    >
+      <Grid templateColumns="repeat(5, 1fr)">
+        {data.navContents.map((navContent) =>
+          navContent.navColumns.map((navColumn, navColumnIndex) => (
+            <Stack key={navColumnIndex} spacing={0}>
+              {navColumn.categories.map((navColumn, navColumnIndex) => (
+                <Box key={navColumnIndex}>
+                  {navColumn.categories.map(
+                    (navGroup: any, navGroupIndex: number) => (
+                      <Box key={navGroupIndex} pt={"7px"}>
+                        <Box
+                          fontSize={"13px"}
+                          fontWeight={"bold"}
+                          py={"2px"}
+                          color={"#ee5f73"}
+                        >
+                          <a href={navGroup.header.url}>
+                            {navGroup.header.label}
+                          </a>
+                        </Box>
+                        <Stack spacing={0}>
+                          {navGroup.menuItems?.map((navGroupItem: any) => (
+                            <NavItem data={navGroupItem} />
                           ))}
-                      {navGroup.menuItems? <Divider  w={"60%"} p={'5px'} /> : ""}
-                      </Stack>
-                    </Box>
-                    ))}
-                  </Box>
-                ))}
-              </Stack>
-            ))}
-          </Grid>
-        </Box>
-  )
-}
+                        </Stack>
+                      </Box>
+                    )
+                  )}
+                </Box>
+              ))}
+            </Stack>
+          ))
+        )}
+      </Grid>
+    </Box>
+  );
+};
 
-export default DropdownNavItems
-
+export default DropdownNavItems;
--- src/components/NavbarTypeItems.tsx
+++ src/components/NavbarTypeItems.tsx
@@ -1,26 +1,30 @@
-import React, {useState} from 'react'
-import {
-  HStack,
-  Stack,
-  Text, Box
-} from '@chakra-ui/react'
-import DropdownNavItems from './DropdownNavItems';
+import React, { useState } from "react";
+import { HStack, Text } from "@chakra-ui/react";
+import DropdownNavItems from "./DropdownNavItems";
+import { CategoryOptions } from "../constants/navbarOptionsData";
 
-const NavbarTypeItems= ({data}:any) => {
+const NavbarTypeItems = ({ data }: { data: CategoryOptions }) => {
   const [dropdown, setDropdown] = useState(false);
   const [open, setOpen] = React.useState(false);
 
   return (
-    <HStack w={'35%'} justifyContent={'space-between'}>
-      {data.header.map((navItems, navItemsIndex) => (
-      <Text key={navItemsIndex} fontSize={'xs'} fontWeight={'bold'}  margin={'0'}  _hover={{ bgGradient:'linear(to-l, green, orange)', bgClip:'text' }}
-        onClick={() => setOpen((!open))} position='relative'>{navItems.label}</Text>
+    <HStack w={"35%"} justifyContent={"space-between"}>
+      {data.navContents.map((navContent, navContentIndex) => (
+        <Text
+          key={navContentIndex}
+          fontSize={"xs"}
+          fontWeight={"bold"}
+          margin={"0"}
+          _hover={{ bgGradient: "linear(to-l, green, orange)", bgClip: "text" }}
+          onClick={() => setOpen(!open)}
+          position="relative"
+        >
+          {navContent.header.label}
+        </Text>
       ))}
-        {open ? (
-        <DropdownNavItems data={data}/>
-        ) : ""}
+      {open ? <DropdownNavItems data={data} /> : ""}
     </HStack>
-  )
-}
+  );
+};
 
-export default NavbarTypeItems
+export default NavbarTypeItems;
