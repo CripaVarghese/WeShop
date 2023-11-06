67df109f303024f35d8eeb2f0c8bdb9a6149b2bf
Cripa Varghese <cripav96@gmail.com>
2023-11-06 23:30:19 +0530

    feature: added the component

--- src/components/DisplayCategory.tsx
+++ src/components/DisplayCategory.tsx
@@ -1,7 +1,51 @@
-import { Stack } from "@chakra-ui/react";
+import { Grid, GridItem, Stack, Image, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import Wishlist from "./Wishlist";
 
 const DisplayCategory = () => {
-  return <Stack>DisplayCategory</Stack>;
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+  return (
+    <Stack w={"80%"} h={"420px"} p={"20px"}>
+      <Grid
+        templateColumns="repeat(5, 1fr)"
+        gap={5}
+        h={"100%"}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        position={"relative"}
+      >
+        <GridItem
+          w="100%"
+          _hover={{
+            boxShadow:
+              "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
+          }}
+        >
+          <Image src="https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/12377258/2020/9/11/ce1b7bcb-a65a-4eb0-a317-42ac02718f1e1599798741705UrbanoFashionPrintedMenRoundNeckDarkGreenT-Shirt1.jpg" />
+          <Stack px={"10px"} mt={"12px"} spacing={".5px"}>
+            <Text fontSize={"sm"} fontWeight={"bold"}>
+              Urbano Fashion
+            </Text>
+            <Text fontSize={"13px"} color={"#535766"}>
+              Slim Tropical Printed Cotton
+            </Text>
+            <Text fontSize={"xs"} fontWeight={"bold"} pt={"6px"}>
+              Rs 450
+            </Text>
+          </Stack>
+        </GridItem>
+      </Grid>
+      {isHovering && <Wishlist />}
+    </Stack>
+  );
 };
 
 export default DisplayCategory;
