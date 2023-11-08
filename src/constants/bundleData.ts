e7f90bbdaedcdf6a64f883dd8671c05a70684975
Cripa Varghese <cripav96@gmail.com>
2023-11-08 23:52:10 +0530

    feature: added data

---
+++ src/constants/bundleData.ts
@@ -0,0 +1,141 @@
+export interface bundleDataType {
+  menu: {
+    header: string;
+    menuItem: {
+      label: string;
+      url: string;
+    }[];
+  }[];
+}
+
+export const bundleData: bundleDataType = {
+  menu: [
+    {
+      header: "bundle",
+      menuItem: [
+        {
+          label: "Bundle Item",
+          url: "#",
+        },
+        {
+          label: "Bundle Item",
+          url: "#",
+        },
+      ],
+    },
+    {
+      header: "Country of Origin",
+      menuItem: [
+        {
+          label: "Bundle Item",
+          url: "#",
+        },
+        {
+          label: "Bundle Item",
+          url: "#",
+        },
+      ],
+    },
+    {
+      header: "Size",
+      menuItem: [
+        {
+          label: "3XS",
+          url: "",
+        },
+        {
+          label: "XXS",
+          url: "",
+        },
+        {
+          label: "XS",
+          url: "",
+        },
+        {
+          label: "XS/S",
+          url: "",
+        },
+        {
+          label: "S",
+          url: "",
+        },
+        {
+          label: "S/M",
+          url: "",
+        },
+        {
+          label: "M",
+          url: "",
+        },
+        {
+          label: "M/L",
+          url: "",
+        },
+        {
+          label: "L",
+          url: "",
+        },
+        {
+          label: "L/XL",
+          url: "",
+        },
+        {
+          label: "XL",
+          url: "",
+        },
+        {
+          label: "XL/XXL",
+          url: "",
+        },
+        {
+          label: "XXL",
+          url: "",
+        },
+        {
+          label: "3XL",
+          url: "",
+        },
+        {
+          label: "4XL",
+          url: "",
+        },
+        {
+          label: "5XL",
+          url: "",
+        },
+        {
+          label: "6XL",
+          url: "",
+        },
+        {
+          label: "7XL",
+          url: "",
+        },
+        {
+          label: "8XL",
+          url: "",
+        },
+        {
+          label: "9XL",
+          url: "",
+        },
+        {
+          label: "10XL",
+          url: "",
+        },
+        {
+          label: "11XL",
+          url: "",
+        },
+        {
+          label: "Onesize",
+          url: "",
+        },
+        {
+          label: "XXXL",
+          url: "",
+        },
+      ],
+    },
+  ],
+};
