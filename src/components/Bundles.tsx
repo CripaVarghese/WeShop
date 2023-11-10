cbbe271a90d0a35912fd4a578e1d3ed98c358aae
Cripa Varghese <cripav96@gmail.com>
2023-11-10 23:54:38 +0530

    feature: added mapping

--- src/components/Bundles.tsx
+++ src/components/Bundles.tsx
@@ -2,22 +2,33 @@ import { ChevronDownIcon } from "@chakra-ui/icons";
 import { HStack, Text } from "@chakra-ui/react";
 import React from "react";
 import BundleMenu from "./BundleMenu";
+import { font } from "../style";
+import { bundleData } from "../constants/bundleData";
 
 const Bundles = () => {
   const [open, setOpen] = React.useState(false);
+  const handleClick = () => {
+    setOpen(!open);
+  };
   return (
-    <HStack justifyContent={"space-between"} w={"30%"}>
-      <HStack
-        p={"7px 12px"}
-        borderRadius={"15px"}
-        _hover={{ bgColor: "#F5F5F5" }}
-        position="absolute"
-        onClick={() => setOpen(!open)}
-      >
-        <Text>Bundles</Text>
-        <ChevronDownIcon />
+    <HStack {...font} w={"65%"}>
+      <HStack w={"35%"} position="relative">
+        {bundleData.menu.map((item) => (
+          <>
+            <HStack
+              p={"7px 12px"}
+              key={item.id}
+              borderRadius={"15px"}
+              _hover={{ bgColor: "#F5F5F5" }}
+              onClick={handleClick}
+            >
+              <Text>{item.header}</Text>
+              <ChevronDownIcon />
+            </HStack>
+            {open ? <BundleMenu data={item?.menuItem} /> : ""}
+          </>
+        ))}
       </HStack>
-      {open ? <BundleMenu /> : ""}
     </HStack>
   );
 };
