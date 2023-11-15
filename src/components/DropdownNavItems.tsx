e11811548540eb5254f179b770694a99560509ea
Cripa Varghese <cripav96@gmail.com>
2023-11-15 23:19:02 +0530

    feature: added color

--- src/components/DropdownNavItems.tsx
+++ src/components/DropdownNavItems.tsx
@@ -1,5 +1,4 @@
-import { Stack, Box, Grid, Divider } from "@chakra-ui/react";
-import { CategoryOptions } from "../constants/navbarOptionsData";
+import { Stack, Box, Grid } from "@chakra-ui/react";
 
 const NavItem = (props: {
   data: { url?: string; label: any };
@@ -16,48 +15,53 @@ const NavItem = (props: {
   </Box>
 );
 
-const DropdownNavItems = ({ data }: { data: CategoryOptions }) => {
+const DropdownNavItems = ({ data, color }: any) => {
   return (
     <Box
       position="absolute"
-      top="80px"
+      top="72px"
       right="100px"
       width="83%"
       height="440px"
       backgroundColor="white"
       p={"15px 40px"}
+      boxShadow={
+        "rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px"
+      }
     >
       <Grid templateColumns="repeat(5, 1fr)">
-        {data.navContents.map((navContent) =>
-          navContent.navColumns.map((navColumn, navColumnIndex) => (
+        {data.map(
+          (navColumn: { categories: any[] }, navColumnIndex: string) => (
             <Stack key={navColumnIndex} spacing={0}>
-              {navColumn.categories.map((navColumn, navColumnIndex) => (
-                <Box key={navColumnIndex}>
-                  {navColumn.categories.map(
-                    (navGroup: any, navGroupIndex: number) => (
-                      <Box key={navGroupIndex} pt={"7px"}>
-                        <Box
-                          fontSize={"13px"}
-                          fontWeight={"bold"}
-                          py={"2px"}
-                          color={"#ee5f73"}
-                        >
-                          <a href={navGroup.header.url}>
-                            {navGroup.header.label}
-                          </a>
+              {navColumn.categories.map(
+                (navColumn: { categories: any[] }, navColumnIndex: string) => (
+                  <Box key={navColumnIndex}>
+                    {navColumn.categories.map(
+                      (navGroup: any, navGroupIndex: number) => (
+                        <Box key={navGroupIndex} pt={"7px"}>
+                          <Box
+                            fontSize={"13px"}
+                            fontWeight={"bold"}
+                            py={"2px"}
+                            color={color}
+                          >
+                            <a href={navGroup.header.url}>
+                              {navGroup.header.label}
+                            </a>
+                          </Box>
+                          <Stack spacing={0}>
+                            {navGroup.menuItems?.map((navGroupItem: any) => (
+                              <NavItem data={navGroupItem} />
+                            ))}
+                          </Stack>
                         </Box>
-                        <Stack spacing={0}>
-                          {navGroup.menuItems?.map((navGroupItem: any) => (
-                            <NavItem data={navGroupItem} />
-                          ))}
-                        </Stack>
-                      </Box>
-                    )
-                  )}
-                </Box>
-              ))}
+                      )
+                    )}
+                  </Box>
+                )
+              )}
             </Stack>
-          ))
+          )
         )}
       </Grid>
     </Box>
