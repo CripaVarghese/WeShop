0b4e25c4b304f449cdef22c95e5948fbb302a096
Cripa Varghese <cripav96@gmail.com>
2023-11-15 23:22:10 +0530

    feature: added hover and styles

--- src/components/NavbarTypeItems.tsx
+++ src/components/NavbarTypeItems.tsx
@@ -1,28 +1,57 @@
-import React from "react";
-import { HStack, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import { HStack, Stack, Text, Box } from "@chakra-ui/react";
 import DropdownNavItems from "./DropdownNavItems";
 import { CategoryOptions } from "../constants/navbarOptionsData";
 
 const NavbarTypeItems = ({ data }: { data: CategoryOptions }) => {
-  const [open, setOpen] = React.useState(false);
-  const [selectedMenuId, setSelectedMenuId] = React.useState(null);
+  const [selectedMenuId, setSelectedMenuId] = useState(null);
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = (id: string | null) => {
+    if (selectedMenuId === id) {
+      setSelectedMenuId(null);
+    } else {
+      setSelectedMenuId(id);
+      setIsHovering(true);
+    }
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
 
   return (
     <HStack w={"35%"} justifyContent={"space-between"}>
       {data.navContents.map((navContent) => (
-        <Text
-          key={navContent.header.id}
-          fontSize={"xs"}
-          fontWeight={"bold"}
-          margin={"0"}
-          _hover={{ bgGradient: "linear(to-l, green, orange)", bgClip: "text" }}
-          onClick={() => setOpen(!open)}
-          position="relative"
-        >
-          {navContent.header.label}
-        </Text>
+        <Stack key={navContent.header.id} justifyContent={"flex-end"}>
+          <Stack
+            onMouseOver={() => handleMouseOver(navContent.header.id)}
+            onMouseOut={handleMouseOut}
+          >
+            <Box
+              mt={"28px"}
+              pb={"28px"}
+              px={"17px"}
+              height={"43px"}
+              _hover={{
+                borderBottom: `2px solid ${navContent.header.color}`,
+              }}
+            >
+              <Text fontSize={"xs"} fontWeight={"bold"} position="relative">
+                {navContent.header.label}
+              </Text>
+            </Box>
+          </Stack>
+          {isHovering && selectedMenuId === navContent.header.id ? (
+            <DropdownNavItems
+              data={navContent.navColumns}
+              color={navContent.header.color}
+            />
+          ) : (
+            ""
+          )}
+        </Stack>
       ))}
-      {open ? <DropdownNavItems data={data} /> : ""}
     </HStack>
   );
 };
