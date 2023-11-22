6522fbe8d721a0f6ad8a63e28f3efa79c5926364
Cripa Varghese <cripav96@gmail.com>
2023-11-22 22:47:18 +0530

    feat: added a useState

--- src/components/NavbarTypeItems.tsx
+++ src/components/NavbarTypeItems.tsx
@@ -4,45 +4,44 @@ import DropdownNavItems from "./DropdownNavItems";
 import { CategoryOptions } from "../constants/navbarOptionsData";
 
 const NavbarTypeItems = ({ data }: { data: CategoryOptions }) => {
-  const [selectedMenuId, setSelectedMenuId] = useState(null);
-  const [isHovering, setIsHovering] = useState(false);
+  const [selectedMenuId, setSelectedMenuId] = useState<CategoryOptions | null>(
+    null
+  );
+  const [isVisible, setIsVisible] = useState(false);
 
   const handleMouseOver = (id: string | null) => {
-    if (selectedMenuId === id) {
-      setSelectedMenuId(null);
-    } else {
-      setSelectedMenuId(id);
-      setIsHovering(true);
-    }
+    setSelectedMenuId(id);
+    setIsVisible(true);
   };
 
   const handleMouseOut = () => {
-    setIsHovering(false);
+    setIsVisible(false);
   };
 
   return (
     <HStack w={"35%"} justifyContent={"space-between"}>
       {data.navContents.map((navContent) => (
-        <Stack key={navContent.header.id} justifyContent={"flex-end"}>
-          <Stack
-            onMouseOver={() => handleMouseOver(navContent.header.id)}
-            onMouseOut={handleMouseOut}
+        <Stack
+          key={navContent.header.id}
+          justifyContent={"flex-end"}
+          onMouseOver={() => handleMouseOver(navContent.header.id)}
+          onMouseOut={handleMouseOut}
+        >
+          <Box
+            mt={"28px"}
+            pb={"28px"}
+            px={"17px"}
+            height={"43px"}
+            _hover={{
+              borderBottom: `2px solid ${navContent.header.color}`,
+            }}
+            position="relative"
           >
-            <Box
-              mt={"28px"}
-              pb={"28px"}
-              px={"17px"}
-              height={"43px"}
-              _hover={{
-                borderBottom: `2px solid ${navContent.header.color}`,
-              }}
-            >
-              <Text fontSize={"xs"} fontWeight={"bold"} position="relative">
-                {navContent.header.label}
-              </Text>
-            </Box>
-          </Stack>
-          {isHovering && selectedMenuId === navContent.header.id ? (
+            <Text fontSize={"xs"} fontWeight={"bold"}>
+              {navContent.header.label}
+            </Text>
+          </Box>
+          {isVisible && selectedMenuId === navContent.header.id ? (
             <DropdownNavItems
               data={navContent.navColumns}
               color={navContent.header.color}
