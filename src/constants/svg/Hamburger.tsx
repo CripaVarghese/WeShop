4e2c3532123f12e2f93a5032a85ce6a0068eddf0
Cripa Varghese <cripav96@gmail.com>
2023-11-28 13:20:43 +0530

    feat: added svg icons

---
+++ src/constants/svg/Hamburger.tsx
@@ -0,0 +1,13 @@
+const Hamburger = () => {
+  return (
+    <svg viewBox="0 0 24 24" width={"25px"}>
+      <path
+        d="M3.444 5.422a.733.733 0 110-1.466h17.047a.733.733 0 110 1.466H3.444zm0 7.111a.733.733 0 110-1.466h17.047a.733.733 0 110 1.466H3.444zm0 7.111a.733.733 0 110-1.466h17.047a.733.733 0 110 1.466H3.444z"
+        fill="#3E4152"
+        fillRule="evenodd"
+      ></path>
+    </svg>
+  );
+};
+
+export default Hamburger;
