cf018e61782a9b292d0242eafd9a27865910fc68
Cripa Varghese <cripav96@gmail.com>
2023-11-29 23:14:46 +0530

    feat: added Dropdown for navbar

---
+++ src/components/main-navbar-menu/navbar/dropdown/DropdownNavItems.tsx
@@ -0,0 +1,73 @@
+import { Stack, Box, Grid, Link } from "@chakra-ui/react";
+
+const NavItem = (props: {
+  data: { url?: string; label: any };
+  color?: string;
+}) => (
+  <Box
+    fontSize={"13px"}
+    py={"2px"}
+    _hover={{ fontWeight: "bold" }}
+    marginTop={0}
+    color={props.color}
+  >
+    <a href={props.data.url ?? "#"}>{props.data.label}</a>
+  </Box>
+);
+
+const DropdownNavItems = ({ data, color }: any) => {
+  return (
+    <Box
+      position="absolute"
+      top="72px"
+      right="100px"
+      width="83%"
+      height="440px"
+      backgroundColor="white"
+      p={"15px 40px"}
+      boxShadow={
+        "rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px"
+      }
+    >
+      <Grid templateColumns="repeat(5, 1fr)">
+        {data.map(
+          (navColumn: { categories: any[] }, navColumnIndex: string) => (
+            <Stack key={navColumnIndex} spacing={0}>
+              {navColumn.categories.map(
+                (navColumn: { categories: any[] }, navColumnIndex: number) => (
+                  <Box key={navColumnIndex}>
+                    {navColumn.categories.map(
+                      (navGroup: any, navGroupIndex: number) => (
+                        <Box key={navGroupIndex} pt={"7px"}>
+                          <Box
+                            fontSize={"13px"}
+                            fontWeight={"bold"}
+                            py={"2px"}
+                            color={color}
+                          >
+                            <a href={navGroup.header.url}>
+                              {navGroup.header.label}
+                            </a>
+                          </Box>
+                          <Stack spacing={0}>
+                            {navGroup.menuItems?.map((navGroupItem: any) => (
+                              <Link _hover={{ textDecoration: "none" }}>
+                                <NavItem data={navGroupItem} />
+                              </Link>
+                            ))}
+                          </Stack>
+                        </Box>
+                      )
+                    )}
+                  </Box>
+                )
+              )}
+            </Stack>
+          )
+        )}
+      </Grid>
+    </Box>
+  );
+};
+
+export default DropdownNavItems;
