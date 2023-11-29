c03a77e638022a1f6886e8cb78fe4236c0f7e9d7
Cripa Varghese <cripav96@gmail.com>
2023-11-29 23:20:30 +0530

    feat: added data for slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,60 +1,75 @@
 import CarouselSlider from "react-carousel-slider";
+let data = [
+  {
+    des: "1",
+    imgSrc:
+      "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/12377258/2020/9/11/ce1b7bcb-a65a-4eb0-a317-42ac02718f1e1599798741705UrbanoFashionPrintedMenRoundNeckDarkGreenT-Shirt1.jpg",
+  },
+  {
+    des: "2",
+    imgSrc: "https://i.imgur.com/pgCzueK.jpg",
+  },
+  {
+    des: "3",
+    imgSrc: "https://i.imgur.com/7nbAJ0C.jpg",
+  },
+  {
+    des: "4",
+    imgSrc: "https://i.imgur.com/L75otV6.jpg",
+  },
+  {
+    des: "5",
+    imgSrc: "https://i.imgur.com/qkPMr9D.jpg",
+  },
+];
 
-export default function Slider() {
-  let data = [
-    {
-      id: "1",
-      imgSrc:
-        "https://cf-images.ap-southeast-1.prod.boltdns.net/v1/jit/5745608584001/262ae93c-443f-4bdc-a11d-71f9fb5b4eb6/main/1280x720/17s514ms/match/image.jpg",
-    },
-    {
-      id: "2",
-      imgSrc:
-        "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2023/10/16/a4959e60-008e-4296-8118-f25eb689e0511697433850966-Virat-s_Favourite_picks___Min_50_Off-_Wrogn.png",
-    },
-    {
-      id: "3",
-      imgSrc:
-        "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2023/10/18/0ad75db8-98f7-4197-b70b-feb1251879e91697616839441-image_png_1590114644.png",
-    },
-    {
-      id: "4",
-      imgSrc:
-        "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2023/10/16/a4959e60-008e-4296-8118-f25eb689e0511697433850966-Virat-s_Favourite_picks___Min_50_Off-_Wrogn.png",
-    },
-    {
-      id: "5",
-      imgSrc:
-        "https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2023/10/18/0ad75db8-98f7-4197-b70b-feb1251879e91697616839441-image_png_1590114644.png",
-    },
-  ];
-  let sliderBoxStyle = {
-    height: "250px",
-    width: "200px",
-  };
+let sliderBoxStyle = {
+  height: "250px",
+  // width: "200px",
+  // , background: "tranparent"
+};
 
-  let itemsStyle = {};
+let itemsStyle = {
+  // ,height: "100%", padding: "0px"
+  // , padding: "15px"
+  // , background: "#FFCA28"
+  // , borderRadius: "4px"
+  // , margin: "0px 0px", padding: "0px"
+};
 
-  let textBoxStyle = {};
+let textBoxStyle = {
+  // textAlign: "left"
+  // ,width:"50%"
+  // , background: "transparent"
+  // , fontSize: "36px"
+  // , fontWeight: 300
+};
 
-  let buttonSetting = {
-    placeOn: "middle-inside",
-  };
+let buttonSetting = {
+  // placeOn: "middle-inside"
+  hoverEvent: true,
+  // , style: {
+  //   left: {
+  //     margin: "0px 0px 0px 10px"
+  //   },
+  //   right: {
+  //     margin: "0px 10px 0px 0px"
+  //   }
+  // }
+};
 
-  let manner = {
-    autoSliding: { interval: "1s" },
-  };
+let manner = {
+  autoSliding: { interval: "2s" },
+  //, duration: "0.3s"
+};
 
-  return (
-    <div className="CarouselSlider">
-      <CarouselSlider
-        slideItems={data}
-        manner={manner}
-        buttonSetting={buttonSetting}
-        sliderBoxStyle={sliderBoxStyle}
-        itemsStyle={itemsStyle}
-        textBoxStyle={textBoxStyle}
-      />
-    </div>
-  );
-}
+export const Slider = () => (
+  <CarouselSlider
+    slideItems={data}
+    manner={manner}
+    buttonSetting={buttonSetting}
+    sliderBoxStyle={sliderBoxStyle}
+    itemsStyle={itemsStyle}
+    textBoxStyle={textBoxStyle}
+  />
+);
