f98762c0471c257c075eb395fc4986b1154c5530
Cripa Varghese <cripav96@gmail.com>
2023-12-02 22:32:05 +0530

    feat: added route

--- src/routes/UnauthenticatedRoutes.tsx
+++ src/routes/UnauthenticatedRoutes.tsx
@@ -2,8 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "../App";
 import ErrorPage from "../components/ErrorPage";
 import Bag_Page from "../components/bag/Bag_Page";
-import Category from "../components/Category";
+import Category from "../components/category/Category";
 import Header from "../components/header/Header";
+import SelectedProductDetails from "../components/selected_product_details/SelectedProductDetails";
+import Wishlist_Page from "../components/wishList/Wishlist_Page";
+import Wishlist_prodAdded from "../components/wishList/Wishlist_prodAdded";
+import Bag_Empty from "../components/bag/Bag_Empty";
 
 export const UnauthenticatedRoutes = () => {
   return (
@@ -13,6 +17,15 @@ export const UnauthenticatedRoutes = () => {
         <Route path="/" element={<App />} />
         <Route path="men" element={<Category />} />
         <Route path="Bag_Page" element={<Bag_Page />} />
+        <Route path="Bag_Empty" element={<Bag_Empty />} />
+        Bag_Empty
+        <Route path="Wishlist_Page" element={<Wishlist_Page />} />
+        <Route path="Wishlist_prodAdded" element={<Wishlist_prodAdded />} />
+        <Route path="Wishlist_prodAdded" element={<Wishlist_prodAdded />} />
+        <Route
+          path="SelectedProductDetails"
+          element={<SelectedProductDetails />}
+        />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
