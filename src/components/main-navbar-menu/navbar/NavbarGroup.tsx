3485b2c80768818f8ca070b3ce60d46144e7b0c5
Cripa Varghese <cripav96@gmail.com>
2023-12-02 21:19:23 +0530

    feat: deleted

--- src/components/main-navbar-menu/navbar/NavbarGroup.tsx
+++
@@ -1,8 +0,0 @@
-import { Stack } from "@chakra-ui/react";
-import React from "react";
-
-const NavbarGroup = () => {
-  return <div>NavbarGroup</div>;
-};
-
-export default NavbarGroup;
--- src/components/main-navbar-menu/navbar/NavbarGroupItem.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NavbarGroupItem = () => {
-  return <div>NavbarGroupItem</div>;
-};
-
-export default NavbarGroupItem;
