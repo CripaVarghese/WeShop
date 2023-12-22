11392c1dedd919fba1847308aa01491968f15018
cripav <cripav96@gmail.com>
2023-12-22 23:59:02 +0530

    feat: added useContext

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import ShopContextProvider from "./context/ShopCartContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <ShopContextProvider>
+        <App />
+      </ShopContextProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
