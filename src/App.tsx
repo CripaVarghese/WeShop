5a835c2c769a3621b25263813a8af72508ecbe64
cripav <cripav96@gmail.com>
2024-01-11 00:54:18 +0530

    feat: added Route for BagPage and WishlistPage components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ErrorPage from "./components/ErrorPage";
-import BagPage from "./components/bag/BagPage";
 import Category from "./components/category/Category";
 import Header from "./components/header/Header";
 import SelectedProductDetails from "./components/selected_product_details/SelectedProductDetails";
-import WishlistPage from "./components/wishList/WishlistPage";
 import WishlistProdAdded from "./components/wishList/WishlistProdAdded";
 import BagEmpty from "./components/bag/BagEmpty";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
-import ProductInCart from "./components/category/ProductInCart";
+import WishlistEmpty from "./components/wishList/WishlistEmpty";
+import WishlistPage from "./components/wishList/WishListPage";
+import BagProdAdded from "./components/bag/BagProdAdded";
+import BagPage from "./components/bag/BagPage";
 
 export const App = () => {
   return (
@@ -19,8 +20,10 @@ export const App = () => {
         <Route path="/" element={<Hero />} />
         <Route path="/men" element={<Category />} />
         <Route path="/bag-page" element={<BagPage />} />
+        <Route path="/bag-product-added" element={<BagProdAdded />} />
         <Route path="/bag-empty" element={<BagEmpty />} />
         <Route path="/wishlist-page" element={<WishlistPage />} />
+        <Route path="/wishlist-empty" element={<WishlistEmpty />} />
         <Route path="/wishlist-prodAdded" element={<WishlistProdAdded />} />
         <Route
           path="/selected-product-details"
